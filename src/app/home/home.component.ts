1a7a1eef4f5fa498ae107d5039068750cd410e22
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-08 22:06:17 +0530

    chore: Update HomeComponent to match import style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core'
- 
-import { Page } from '@nativescript/core'
- 
-@Component({
-  selector: 'Home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent implements OnInit {
-   
+import { Component, OnInit } from "@angular/core";
 
+import { Page } from "@nativescript/core";
+
+@Component({
+  selector: "Home",
+  templateUrl: "./home.component.html",
+})
+export class HomeComponent{
   // constructor(private _itemService: DataService) {}
   constructor(private _page: Page) {
     this._page.actionBarHidden = true;
-}
+  }
 
   ngOnInit(): void {
     // this.items = this._itemService.getItems()
