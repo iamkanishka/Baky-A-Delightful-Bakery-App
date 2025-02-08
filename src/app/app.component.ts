56c7baaea556183e188eebfb9780822e74cf3683
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-08 21:54:16 +0530

    chore: Update  to add TabView reference and set native properties`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { Color, TabView } from "@nativescript/core";
 
 @Component({
@@ -7,12 +7,25 @@ import { Color, TabView } from "@nativescript/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent  {
+  @ViewChild("tabViewRef", { static: false }) tabView!: ElementRef<TabView>;
+
 
   constructor() {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     
+  }
+
+  ngAfterViewInit() {
+    if (this.tabView) {
+      const nativeTabView = this.tabView.nativeElement;
+      nativeTabView.selectedTabTextColor = new Color("#8B4513"); // Active Tab Title Color
+      nativeTabView.tabTextColor = new Color("black"); // Inactive Tab Title Color
+      nativeTabView.tabTextFontSize = 10; // Tab Title Font Size
+
+      nativeTabView.backgroundColor = new Color("white"); // Tab Background Color
+    }
   }
 
 }
