b8fb699c57937fd4d061d49bd79be1eb10b48d41
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-09 22:12:24 +0530

    Added redirect to default

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { NativeScriptRouterModule } from '@nativescript/angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'default', pathMatch: 'full' },
+  { path: 'default', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [NativeScriptRouterModule.forChild(routes)],
+  exports: [NativeScriptRouterModule]
 })
 export class AuthRoutingModule { }
