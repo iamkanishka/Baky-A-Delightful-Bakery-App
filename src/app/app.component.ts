a863e9eeeabca1e30592320f7ebfe3c6c6191b77
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-09 22:11:32 +0530

    Removed unused funcationalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,5 @@ import { Color, TabView } from "@nativescript/core";
   templateUrl: "app.component.html",
 })
 export class AppComponent {
-  @ViewChild("tabViewRef", { static: false }) tabView!: ElementRef<TabView>;
-
-  constructor() {}
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-  }
-
-  selectedIndex = 0;
-
-  onTabChange(event: any) {
-    this.selectedIndex = event.value;
-  }
-
-  ngAfterViewInit() {
-    if (this.tabView) {
-      const nativeTabView = this.tabView.nativeElement;
-      nativeTabView.selectedTabTextColor = new Color("#8B4513"); // Active Tab Title Color
-      nativeTabView.tabTextColor = new Color("black"); // Inactive Tab Title Color
-      nativeTabView.tabTextFontSize = 10; // Tab Title Font Size
-
-      nativeTabView.backgroundColor = new Color("white"); // Tab Background Color
-    }
-  }
+  
 }
