9a4b22cd152519c31dcee3aa4aec071e8264c3d7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-09 14:14:44 +0530

    chore: Hide Action bar

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from "@angular/core";
+import { Page } from "@nativescript/core";
 
 @Component({
-  selector: 'Browse',
-  templateUrl: './browse.component.html',
+  selector: "Browse",
+  templateUrl: "./browse.component.html",
 })
-export class BrowseComponent implements OnInit {
-  constructor() {
-    // Use the component constructor to inject providers.
-  }
-
-  ngOnInit(): void {
-    // Use the "ngOnInit" handler to initialize data for the view.
+export class BrowseComponent {
+  constructor(private _page: Page) {
+    this._page.actionBarHidden = true;
   }
 }
