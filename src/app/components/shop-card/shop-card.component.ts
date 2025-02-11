d29a74c36c42b83c4b714c31a1b1e85c25cdc171
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-11 21:52:43 +0530

    ShopCard Component

---
+++ src/app/components/shop-card/shop-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-card',
+  imports: [],
+  templateUrl: './shop-card.component.html',
+  styleUrl: './shop-card.component.scss'
+})
+export class ShopCardComponent {
+
+}
