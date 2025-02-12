7763091a4931f9d170895486fafd059635c1720d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-12 22:14:51 +0530

    chore: Update auth-routing.module.ts to use new SignIn and SignUpcomponent names

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { NativeScriptRouterModule } from '@nativescript/angular';
 
 const routes: Routes = [
   { path: '', redirectTo: 'default', pathMatch: 'full' },
-  { path: 'default', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'default', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
 
 ];
 
