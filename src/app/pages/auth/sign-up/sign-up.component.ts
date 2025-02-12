e5768bb107806678fcf2fb7991fd3c02f4e87119
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-12 22:16:31 +0530

    chore: Added actionBarHidden property to SignUpComponent and imported Page from @nativescript/core

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+ 
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+
+  constructor(private _page: Page) {
+    this._page.actionBarHidden = true;
+  }
+}
 
 }
