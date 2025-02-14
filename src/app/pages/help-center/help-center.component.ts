8a958dfd9fb507a420deea2323558de8f1fd6f11
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:04 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/help-center/help-center.component.ts
+++ src/app/pages/help-center/help-center.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-help-center',
-  imports: [],
+  
   templateUrl: './help-center.component.html',
   styleUrl: './help-center.component.scss'
 })
