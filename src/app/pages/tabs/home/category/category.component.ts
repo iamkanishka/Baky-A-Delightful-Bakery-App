82fca85d57e10ba5cf54de3dd9dc9645418f8c15
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:59 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/tabs/home/category/category.component.ts
+++ src/app/pages/tabs/home/category/category.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
