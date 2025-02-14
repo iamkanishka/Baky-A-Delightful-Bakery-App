cf1c0cd1f2b71a84f00cec5eaa34289a1d7baf0d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:11:34 +0530

    Added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NativeScriptModule } from '@nativescript/angular'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule,HttpClientModule, AppRoutingModule],
   declarations: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
