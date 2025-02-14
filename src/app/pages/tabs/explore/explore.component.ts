94e74dad68f3dcbe0154fbdb7bf20348d153c261
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:20:08 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/tabs/explore/explore.component.ts
+++ src/app/pages/tabs/explore/explore.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-explore',
-  imports: [],
+ 
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
