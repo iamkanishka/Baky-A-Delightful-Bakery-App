40280e077e0a2c27b342d711ffa6447af3ff2177
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:08 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/reviews/leave-a-review/leave-a-review.component.ts
+++ src/app/pages/reviews/leave-a-review/leave-a-review.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-leave-a-review',
-  imports: [],
+  
   templateUrl: './leave-a-review.component.html',
   styleUrl: './leave-a-review.component.scss'
 })
