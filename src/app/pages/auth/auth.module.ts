37405a404171fb21953807dce97f06fce10ce007
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:12:54 +0530

    style: Formatting

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,23 +1,15 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
- 
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { NativeScriptCommonModule } from '@nativescript/angular';
-import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from "./auth-routing.module";
 
-
-
+import { NativeScriptCommonModule } from "@nativescript/angular";
+import { CommonModule } from "@angular/common";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { SignInComponent } from "./sign-in/sign-in.component";
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent],
-  imports: [
-    CommonModule,
-    NativeScriptCommonModule,
-    AuthRoutingModule
-  ],
+  imports: [CommonModule, NativeScriptCommonModule, AuthRoutingModule],
   schemas: [NO_ERRORS_SCHEMA],
-
 })
-export class AuthModule { }
+export class AuthModule {}
