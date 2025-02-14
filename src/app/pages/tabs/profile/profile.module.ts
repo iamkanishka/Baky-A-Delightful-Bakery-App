20caaa4d04c2cc3d16751e66df220c24804fbabd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:28:01 +0530

    chore: Added  NativeScriptCommonModule

--- src/app/pages/tabs/profile/profile.module.ts
+++ src/app/pages/tabs/profile/profile.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { NativeScriptCommonModule } from '@nativescript/angular';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    NativeScriptCommonModule,
     ProfileRoutingModule
   ]
 })
