5dc21bdee859a6592dae9e9fb127e7d98a7b181e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:28:24 +0530

    chore: Added  NativeScriptCommonModule

--- src/app/pages/tabs/chat/chat.module.ts
+++ src/app/pages/tabs/chat/chat.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
+import { ChatComponent } from './chat.component';
+import { NativeScriptCommonModule } from '@nativescript/angular';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ChatComponent],
   imports: [
     CommonModule,
+    NativeScriptCommonModule,
     ChatRoutingModule
   ]
 })
