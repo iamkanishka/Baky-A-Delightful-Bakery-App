4416b725543ad07c671f1a112d1e6a58c9e42711
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:21:22 +0530

    chore: Added Default redirect

--- src/app/pages/tabs/explore/explore-routing.module.ts
+++ src/app/pages/tabs/explore/explore-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { ExploreComponent } from "./explore.component";
+import { NativeScriptRouterModule } from "@nativescript/angular";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "default", pathMatch: "full" },
+  { path: "default", component: ExploreComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [NativeScriptRouterModule.forChild(routes)],
+  exports: [NativeScriptRouterModule],
 })
-export class ExploreRoutingModule { }
+export class ExploreRoutingModule {}
