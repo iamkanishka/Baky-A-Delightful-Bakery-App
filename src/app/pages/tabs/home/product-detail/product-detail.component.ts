e1a928f37dcc667bc90ae115ef295dfe1288f041
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:55 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/tabs/home/product-detail/product-detail.component.ts
+++ src/app/pages/tabs/home/product-detail/product-detail.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
