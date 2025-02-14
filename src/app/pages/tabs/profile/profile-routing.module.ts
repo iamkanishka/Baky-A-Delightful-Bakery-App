5c352997a7485557324dc5b0c4c88378491c7278
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:25:50 +0530

    chore: Added Default redirect

--- src/app/pages/tabs/profile/profile-routing.module.ts
+++ src/app/pages/tabs/profile/profile-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { NativeScriptRouterModule } from '@nativescript/angular';
+import { ProfileComponent } from "./profile.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: "default", pathMatch: "full" },
+  { path: "default", component: ProfileComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [NativeScriptRouterModule.forChild(routes)],
+  exports: [NativeScriptRouterModule]
 })
 export class ProfileRoutingModule { }
