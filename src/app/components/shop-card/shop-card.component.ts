5937e545e5d1d50d54b1b14ddd92d1ed6be8f209
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:14:27 +0530

    chore: Removed empty imports from banner component

--- src/app/components/shop-card/shop-card.component.ts
+++ src/app/components/shop-card/shop-card.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shop-card',
-  imports: [],
+  
   templateUrl: './shop-card.component.html',
   styleUrl: './shop-card.component.scss'
 })
