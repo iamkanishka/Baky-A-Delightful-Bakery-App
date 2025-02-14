6d0e3bf9fd8a24c20f67a72b41aa57afc90df228
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:14:38 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/auth/forgort-password/forgort-password.component.ts
+++ src/app/pages/auth/forgort-password/forgort-password.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-forgort-password',
-  imports: [],
+  
   templateUrl: './forgort-password.component.html',
   styleUrl: './forgort-password.component.scss'
 })
