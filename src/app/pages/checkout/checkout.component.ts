4cba71e5b036bda1d5a59d4db07b155e5a7e805d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:18:58 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
