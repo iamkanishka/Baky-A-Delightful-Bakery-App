5e4a10b5be6504b1116bbd50c7e6e17777e719dd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:24 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/shared/enable-location/enable-location.component.ts
+++ src/app/pages/shared/enable-location/enable-location.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-enable-location',
-  imports: [],
+  
   templateUrl: './enable-location.component.html',
   styleUrl: './enable-location.component.scss'
 })
