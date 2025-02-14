3071dea44069755d5bb1a8f32a21b9cc95b76242
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:15 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/reviews/order-reviews/order-reviews.component.ts
+++ src/app/pages/reviews/order-reviews/order-reviews.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order-reviews',
-  imports: [],
+  
   templateUrl: './order-reviews.component.html',
   styleUrl: './order-reviews.component.scss'
 })
