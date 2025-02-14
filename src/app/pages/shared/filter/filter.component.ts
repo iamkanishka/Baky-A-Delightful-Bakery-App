d9e93fb1b9375137b5c7adc911dfa04a4d2c6b2a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:27 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/shared/filter/filter.component.ts
+++ src/app/pages/shared/filter/filter.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
