5fdf6fee1a4036899514adb5e1f6fba0fd15980c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:20:24 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/tabs/chat/chat.component.ts
+++ src/app/pages/tabs/chat/chat.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
