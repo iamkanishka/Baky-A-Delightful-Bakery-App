832dc7ee045ff2e15c99959cc2018f6719ef9c62
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:14:05 +0530

    chore: Removed empty imports from banner component

--- src/app/components/loaders/banner-loader/banner-loader.component.ts
+++ src/app/components/loaders/banner-loader/banner-loader.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner-loader',
-  imports: [],
+  
   templateUrl: './banner-loader.component.html',
   styleUrl: './banner-loader.component.scss'
 })
