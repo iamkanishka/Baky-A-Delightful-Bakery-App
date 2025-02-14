74b7b1c81942ddbfa841e001035e75b6d2513819
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:14:22 +0530

    chore: Removed empty imports from banner component

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
-  imports: [],
+  
   templateUrl: './rating.component.html',
   styleUrl: './rating.component.scss'
 })
