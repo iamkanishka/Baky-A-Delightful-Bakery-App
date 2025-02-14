e260fbd2f7f67c51599c66c342241fb8a9cd19d8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:18:50 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/auth/verify-code/verify-code.component.ts
+++ src/app/pages/auth/verify-code/verify-code.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-verify-code',
-  imports: [],
+  
   templateUrl: './verify-code.component.html',
   styleUrl: './verify-code.component.scss'
 })
