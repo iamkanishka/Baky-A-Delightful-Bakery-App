a0f361c562a8d4d14403f457e496eb93d722758e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:27:38 +0530

    chore: Added  Common Module

--- src/app/pages/tabs/home/home.module.ts
+++ src/app/pages/tabs/home/home.module.ts
@@ -3,10 +3,11 @@ import { NativeScriptCommonModule } from '@nativescript/angular'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
+import { CommonModule } from '@angular/common'
 
 
 @NgModule({
-  imports: [NativeScriptCommonModule, HomeRoutingModule],
+  imports: [CommonModule ,NativeScriptCommonModule, HomeRoutingModule],
   declarations: [HomeComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
