fe633261cbd9e0cc4a99bbf37fb3bcb245fbe316
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-14 23:19:32 +0530

    chore: Removed empty imports from banner component

--- src/app/pages/special-offers/special-offers.component.ts
+++ src/app/pages/special-offers/special-offers.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-special-offers',
-  imports: [],
+  
   templateUrl: './special-offers.component.html',
   styleUrl: './special-offers.component.scss'
 })
