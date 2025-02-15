5ad00cb3493e49a897217b0ac2162a7c1b45a56c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-15 20:56:08 +0530

    chore: Updated Modules

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
+import { NgModule, OnInit } from "@angular/core";
+import { ActivatedRoute, Routes } from "@angular/router";
 import {
   NativeScriptRouterModule,
   NSEmptyOutletComponent,
 } from "@nativescript/angular";
 
-export const routes: Routes = [
+import { TabsComponent } from "./tabs.component"; // Wrapper component for the tabs
+
+const routes: Routes = [
   {
     path: "",
     redirectTo:
-      "/(homeTab:home/default//exploreTab:explore/default//wishlistTab:wishlist/default//chatTab:chat/default//profileTab:profile/default)",
+      "/tabs/(homeTab:home/default//exploreTab:explore/default//wishlistTab:wishlist/default//chatTab:chat/default//profileTab:profile/default)",
     pathMatch: "full",
   },
 
@@ -19,34 +21,42 @@ export const routes: Routes = [
     loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
     outlet: "homeTab",
   },
-  // {
-  //   path: 'explore',
-  //   component: NSEmptyOutletComponent,
-  //   loadChildren: () => import('./explore/explore.module').then(m => m.ExploreModule),
-  //   outlet: 'exploreTab',
-  // },
-  // {
-  //   path: 'wishlist',
-  //   component: NSEmptyOutletComponent,
-  //   loadChildren: () => import('./wishlist/wishlist.module').then(m => m.WishlistModule),
-  //   outlet: 'wishlistTab',
-  // },
-  // {
-  //   path: 'chat',
-  //   component: NSEmptyOutletComponent,
-  //   loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule),
-  //   outlet: 'chatTab',
-  // },
-  // {
-  //   path: 'profile',
-  //   component: NSEmptyOutletComponent,
-  //   loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
-  //   outlet: 'profileTab',
-  // }
+  {
+    path: "explore",
+    component: NSEmptyOutletComponent,
+    loadChildren: () =>
+      import("./explore/explore.module").then((m) => m.ExploreModule),
+    outlet: "exploreTab",
+  },
+  {
+    path: "wishlist",
+    component: NSEmptyOutletComponent,
+    loadChildren: () =>
+      import("./wishlist/wishlist.module").then((m) => m.WishlistModule),
+    outlet: "wishlistTab",
+  },
+  {
+    path: "chat",
+    component: NSEmptyOutletComponent,
+    loadChildren: () => import("./chat/chat.module").then((m) => m.ChatModule),
+    outlet: "chatTab",
+  },
+  {
+    path: "profile",
+    component: NSEmptyOutletComponent,
+    loadChildren: () =>
+      import("./profile/profile.module").then((m) => m.ProfileModule),
+    outlet: "profileTab",
+  },
 ];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forChild(routes)],
   exports: [NativeScriptRouterModule],
 })
-export class TabsRoutingModule {}
+export class TabsRoutingModule {
+  constructor(private route: ActivatedRoute) {
+    console.log(this.route);
+    console.log("TabsRoutingModule");
+  }
+}
