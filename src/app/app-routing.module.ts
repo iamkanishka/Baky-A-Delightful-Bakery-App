697bd679d3502b4d4bcd53fdcf8499f301b642fb
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-15 20:52:54 +0530

    chore: Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,29 @@
-import { NgModule } from "@angular/core";
+import { NgModule, OnInit } from "@angular/core";
 import { Routes } from "@angular/router";
-import {
-  NativeScriptRouterModule,
-   
-} from "@nativescript/angular";
+import { NativeScriptRouterModule } from "@nativescript/angular";
+
+ 
 
 const routes: Routes = [
-  //  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
-  // { path: '', redirectTo: 'auth', pathMatch: 'full' }
 
   {
+    path: "auth",
+    loadChildren: () => import("./pages/auth/auth.module").then((m) => m.AuthModule),
+  },
+  {
+    path: "",
+    redirectTo: "auth",
+    pathMatch: "full",
+  },
+
+   {
     path: "tabs",
     loadChildren: () =>
       import("./pages/tabs/tabs.module").then((m) => m.TabsModule),
     // canActivate: [AuthGuard] // Protect TabsModule
   },
-  { path: "", redirectTo: "tabs", pathMatch: "full" },
+  // { path: "", redirectTo: "tabs", pathMatch: "full" },
 
-  // {
-  //   path: '',
-  //   redirectTo: '/tabs',
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path: 'auth',
-  //   loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-  // },
-  // {
-  //   path: 'tabs',
-  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsModule),
-  //   canActivate: [AuthGuard] // Protect TabsModule
-  // }
 ];
 
 @NgModule({
