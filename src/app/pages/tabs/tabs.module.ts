a51aebdea779be70fefab5b662306aef71b16b3f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-16 22:15:25 +0530

    chore: Added NO_ERRORS_SCHEMA

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -1,21 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { TabsRoutingModule } from './tabs-routing.module';
-import { NativeScriptCommonModule } from '@nativescript/angular';
+import { TabsRoutingModule } from "./tabs-routing.module";
+import { NativeScriptCommonModule } from "@nativescript/angular";
 
-import { TabsComponent } from './tabs.component';
+import { TabsComponent } from "./tabs.component";
 
 @NgModule({
+  bootstrap: [TabsComponent],
+
   declarations: [TabsComponent],
-  imports: [
-    NativeScriptCommonModule,
-    CommonModule,
-    TabsRoutingModule
-  ],
- 
+  imports: [NativeScriptCommonModule, TabsRoutingModule],
+  schemas: [NO_ERRORS_SCHEMA],
 })
-export class TabsModule { 
+export class TabsModule {
   constructor() {
     console.log("TabsModule");
   }
