737fbf741701b80c885c6eac19d0762ca08bc05c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-17 21:05:33 +0530

    chore: Declared CatergoriesComponent, CategoriesListComponent, HomeHeaderComponent in teh declaration

--- src/app/pages/tabs/home/home.module.ts
+++ src/app/pages/tabs/home/home.module.ts
@@ -1,20 +1,18 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptCommonModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
 
-import { HomeRoutingModule } from './home-routing.module'
-import { HomeComponent } from './home.component'
-import { CommonModule } from '@angular/common'
-import { ActivatedRoute } from '@angular/router'
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import {CatergoriesComponent} from './catergories/catergories.component';
+import {CategoriesListComponent} from '../../../components/categories-list/categories-list.component';
+import {HomeHeaderComponent} from '../../../components/home-header/home-header.component';
 
 
+ 
+ 
 @NgModule({
-  imports: [CommonModule ,NativeScriptCommonModule, HomeRoutingModule],
-  declarations: [HomeComponent],
+  imports: [NativeScriptCommonModule, NativeScriptRouterModule, HomeRoutingModule],
+  declarations: [HomeComponent, CatergoriesComponent, CategoriesListComponent, HomeHeaderComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class HomeModule {
-  constructor(private route: ActivatedRoute) {
-     
-    console.log("HomeModule");
-  }
-}
+export class HomeModule {}
