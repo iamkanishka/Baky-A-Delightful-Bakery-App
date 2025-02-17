77cd4ef09af4c2ecce7e2b73a14fd37520ff1480
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-17 10:59:16 +0530

    chore: Removed Tabs fns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Color, TabView } from "@nativescript/core";
+import { Color, Page, TabView } from "@nativescript/core";
  
 import { Application, isAndroid, isIOS } from "@nativescript/core";
 
@@ -11,12 +11,9 @@ import { Application, isAndroid, isIOS } from "@nativescript/core";
 export class AppComponent  {
   @ViewChild("tabViewRef", { static: false }) tabView!: ElementRef<TabView>;
 
-  constructor(private route: ActivatedRoute) {
-  console.log('Tabs Component constructor');
+  constructor(){}
+ 
 
-  console.log(this.route);
-  
-  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -28,21 +25,6 @@ export class AppComponent  {
     
   }
 
-  selectedIndex = 0;
 
-  onTabChange(event: any) {
-    this.selectedIndex = event.value;
-  }
-
-  ngAfterViewInit() {
-    if (this.tabView) {
-      const nativeTabView = this.tabView.nativeElement;
-      nativeTabView.selectedTabTextColor = new Color("#8B4513"); // Active Tab Title Color
-      nativeTabView.tabTextColor = new Color("black"); // Inactive Tab Title Color
-      nativeTabView.tabTextFontSize = 10; // Tab Title Font Size
-
-      nativeTabView.backgroundColor = new Color("white"); // Tab Background Color
-    }
-  }
   
 }
