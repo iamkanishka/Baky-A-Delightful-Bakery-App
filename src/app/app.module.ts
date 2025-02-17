58bbef685294db4cd7ca93f0250eb44f2e649a6a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-17 20:58:28 +0530

    chore: Added TabsComponent in the declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
 import { HttpClientModule } from '@angular/common/http';
+import { TabsComponent } from './pages/tabs/tabs.component';
  
  
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule,HttpClientModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, TabsComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
