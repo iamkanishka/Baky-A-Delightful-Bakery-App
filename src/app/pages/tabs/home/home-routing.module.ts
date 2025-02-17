56e59e5a0b07c7793d29a26d7d69df5076c0f499
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-17 21:01:26 +0530

    chore: Added categories path

--- src/app/pages/tabs/home/home-routing.module.ts
+++ src/app/pages/tabs/home/home-routing.module.ts
@@ -3,18 +3,15 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "@nativescript/angular";
 
 import { HomeComponent } from "./home.component";
+import { CatergoriesComponent } from "./catergories/catergories.component";
 
 const routes: Routes = [ 
-  { path: '', redirectTo: "default", pathMatch: "full" },
-  { path: "default", component: HomeComponent }
+  { path: "default", component: HomeComponent },
+  { path: "categories", component: CatergoriesComponent }
 ];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forChild(routes)],
   exports: [NativeScriptRouterModule],
 })
-export class HomeRoutingModule {
-  constructor() {
-    console.log("HomeRoutingModule");
-  }
-}
+export class HomeRoutingModule {}
