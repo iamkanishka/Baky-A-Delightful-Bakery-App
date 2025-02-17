129fa5319bd746850ef19f9d480ea5f123cfa77e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-17 21:05:02 +0530

    chore: Added new Banner images URLS

--- src/app/pages/tabs/home/home.component.ts
+++ src/app/pages/tabs/home/home.component.ts
@@ -17,22 +17,10 @@ export class HomeComponent {
   scrollViewRef!: ElementRef;
 
   items = [
-    {
-      text: "Slide 1",
-      imgURL:
-        "https://pouch.jumpshare.com/preview/E1KCYSQQ_JsWlMDRsbkdk5M2UF3YUATW5w8y97Z2C2kG-hQuO1ip_98MrI34tY8yo6Q_bgNmGL9FHGdaZ30sz4Xn7NBQesh8dm22gE79fxQ",
-    },
-    {
-      text: "Slide 2",
-      imgURL:
-        "https://pouch.jumpshare.com/preview/QnvRO-qw3FfXJ2XgWyUQKn-LDO1uc4k8YFfAMWMV0N7quVP_JAsoFYPxvsK9_Bifhm4VH659AMzA1kAoJp1RRTm7DUNrLs3P9wmQ9sIfW40",
-    },
-    {
-      text: "Slide 3",
-      imgURL:
-        "https://pouch.jumpshare.com/preview/UR34CtrKac79yQDQX7kEtPTnPLCa-mPRm8S7r7mP1Zc4JSyrz9N-Vb8t5dDSAbKio6Q_bgNmGL9FHGdaZ30sz57OThsvfocR_2rsbJ8qqOk",
-    },
-    { text: "Slide 4", imgURL: "https://iili.io/2pgQyWg.png" },
+    { text: 'Slide 1', imgURL: "https://iili.io/2yfv1Eu.png" },
+    { text: 'Slide 2', imgURL: "https://iili.io/2yf8f3b.png" },
+    { text: 'Slide 3', imgURL: "https://iili.io/2yfg2nI.jpg" },
+    { text: 'Slide 4', imgURL: "https://iili.io/2yfg0wQ.md.jpg" }
   ];
   screenWidth: number = Screen.mainScreen.widthDIPs;
   currentIndex = 0;
