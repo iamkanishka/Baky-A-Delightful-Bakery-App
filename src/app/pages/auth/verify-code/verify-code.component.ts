8d45fc10c9fb1454c8ca7814ae0b7135d128b60f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-18 21:46:44 +0530

    feat: Added VerifyCodeComponent logic

--- src/app/pages/auth/verify-code/verify-code.component.ts
+++ src/app/pages/auth/verify-code/verify-code.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify-code',
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './verify-code.component.scss'
 })
 export class VerifyCodeComponent {
+
+  otp1: string = '';
+  otp2: string = '';
+  otp3: string = '';
+  otp4: string = '';
+
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/previous-page']);
+  }
+
+  resendCode() {
+    console.log("Resend code tapped");
+  }
+
+  verifyOtp() {
+    const otp = this.otp1 + this.otp2 + this.otp3 + this.otp4;
+    console.log("Entered OTP:", otp);
+    if (otp.length === 4) {
+      // Implement verification logic
+      console.log("OTP verified successfully!");
+    } else {
+      console.log("Please enter a valid 4-digit OTP");
+    }
+  }
 
 }
