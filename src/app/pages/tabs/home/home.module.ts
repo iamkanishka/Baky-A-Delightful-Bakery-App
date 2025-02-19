7791625c870049918c62b509c6e88c37147fb19c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-19 21:51:46 +0530

    chore: Added BannerComponent in declaration

--- src/app/pages/tabs/home/home.module.ts
+++ src/app/pages/tabs/home/home.module.ts
@@ -6,13 +6,13 @@ import { HomeComponent } from './home.component';
 import {CatergoriesComponent} from './catergories/catergories.component';
 import {CategoriesListComponent} from '../../../components/categories-list/categories-list.component';
 import {HomeHeaderComponent} from '../../../components/home-header/home-header.component';
-
+import {BannerComponent} from '../../../components/banner/banner.component';
 
  
  
 @NgModule({
   imports: [NativeScriptCommonModule, NativeScriptRouterModule, HomeRoutingModule],
-  declarations: [HomeComponent, CatergoriesComponent, CategoriesListComponent, HomeHeaderComponent],
+  declarations: [HomeComponent, CatergoriesComponent, CategoriesListComponent, HomeHeaderComponent, BannerComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class HomeModule {}
