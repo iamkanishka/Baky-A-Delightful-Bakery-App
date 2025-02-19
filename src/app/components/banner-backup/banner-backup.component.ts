3f0a58ffe79d56edfc36a3f40e680dff496eace6
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-19 22:00:53 +0530

    BannerBackupComponent init commit

---
+++ src/app/components/banner-backup/banner-backup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-backup',
+  imports: [],
+  templateUrl: './banner-backup.component.html',
+  styleUrl: './banner-backup.component.scss'
+})
+export class BannerBackupComponent {
+
+}
