a5c0a9e0a2ca513c61bc25b78b420a739ab77a02
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-19 21:53:53 +0530

    chore: Added naviagteToCategories fn

--- src/app/components/categories-list/categories-list.component.ts
+++ src/app/components/categories-list/categories-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { Categories } from "../../types/home/categories/index";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-categories-list",
@@ -72,10 +73,18 @@ export class CategoriesListComponent {
     },
   ];
 
-  constructor() {}
+  constructor( private router: Router) {  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.categories = this.categories.slice(0, this.categoriesQT);
   }
+
+  public naviagteToCategories() {
+    this.router.navigate([
+      "/tabs",
+      { outlets: { homeTab: ["home", "categories"] } },
+    ]);
+  }
+
 }
