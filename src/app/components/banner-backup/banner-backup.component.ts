6e6c75d4cdf62260911dbf2c68ef51a34d2dafe7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-19 22:03:42 +0530

    feat: BannerBackupComponent logics

--- src/app/components/banner-backup/banner-backup.component.ts
+++ src/app/components/banner-backup/banner-backup.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Page } from "@nativescript/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Screen, ScrollView } from "@nativescript/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-banner-backup',
@@ -7,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrl: './banner-backup.component.scss'
 })
 export class BannerBackupComponent {
+  constructor(private _page: Page, private router: Router) {
+    this._page.actionBarHidden = true;
+  }
 
+  @ViewChild("carouselScrollView", { static: false })
+  scrollViewRef!: ElementRef;
+
+  items = [
+    { text: 'Slide 1', imgURL: "https://iili.io/2yfv1Eu.png" },
+    { text: 'Slide 2', imgURL: "https://iili.io/2yf8f3b.png" },
+    { text: 'Slide 3', imgURL: "https://iili.io/2yfg2nI.jpg" },
+    { text: 'Slide 4', imgURL: "https://iili.io/2yfg0wQ.md.jpg" }
+  ];
+  screenWidth: number = Screen.mainScreen.widthDIPs;
+  currentIndex = 0;
+
+  ngOnInit() {
+    setInterval(() => this.nextSlide(), 3000); // Auto-slide every 3 seconds
+  }
+
+  nextSlide() {
+    if (!this.scrollViewRef) return;
+
+    const scrollView = this.scrollViewRef.nativeElement as ScrollView;
+    this.currentIndex = (this.currentIndex + 1) % this.items.length;
+
+    scrollView.scrollToHorizontalOffset(
+      this.currentIndex * this.screenWidth,
+      true
+    );
+  }
+
+  onScroll(event: any) {
+    const scrollView = this.scrollViewRef.nativeElement as ScrollView;
+    this.currentIndex = Math.round(
+      scrollView.horizontalOffset / this.screenWidth
+    );
+  }
+
+  public naviagteToCategories() {
+    this.router.navigate([
+      "/tabs",
+      { outlets: { homeTab: ["home", "categories"] } },
+    ]);
+  }
 }
