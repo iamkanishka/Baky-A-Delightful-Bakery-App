747a508a7b2f2d96ad0e2dbb3c3b52613ee3a022
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-20 21:47:22 +0530

    chore: Added FeaturedProductCardComponent in decalration

--- src/app/pages/tabs/home/home.module.ts
+++ src/app/pages/tabs/home/home.module.ts
@@ -7,12 +7,14 @@ import {CatergoriesComponent} from './catergories/catergories.component';
 import {CategoriesListComponent} from '../../../components/categories-list/categories-list.component';
 import {HomeHeaderComponent} from '../../../components/home-header/home-header.component';
 import {BannerComponent} from '../../../components/banner/banner.component';
+import {FeaturedProductCardComponent} from '../../../components/featured-product-card/featured-product-card.component';
+
 
  
  
 @NgModule({
   imports: [NativeScriptCommonModule, NativeScriptRouterModule, HomeRoutingModule],
-  declarations: [HomeComponent, CatergoriesComponent, CategoriesListComponent, HomeHeaderComponent, BannerComponent],
+  declarations: [HomeComponent, CatergoriesComponent, CategoriesListComponent, HomeHeaderComponent, BannerComponent, FeaturedProductCardComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class HomeModule {}
