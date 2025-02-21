94b7690b0501d8c4a4fec0a7f5c95d121a6454ac
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-21 18:38:18 +0530

    chore: Added Products module with lazy loading

--- src/app/pages/tabs/home/home-routing.module.ts
+++ src/app/pages/tabs/home/home-routing.module.ts
@@ -4,10 +4,18 @@ import { NativeScriptRouterModule } from "@nativescript/angular";
 
 import { HomeComponent } from "./home.component";
 import { CatergoriesComponent } from "./catergories/catergories.component";
+import { ProductsListComponent } from "../../products/products-list/products-list.component";
+import { ProductDetailComponent } from "../../products/product-detail/product-detail.component";
 
-const routes: Routes = [ 
-  { path: "default", component: HomeComponent },
-  { path: "categories", component: CatergoriesComponent }
+const routes: Routes = [
+  // { path: "default", component: HomeComponent },
+  { path: "categories", component: CatergoriesComponent },
+  {
+    path: "default",
+    component: ProductDetailComponent,
+    loadChildren: () =>
+      import("../../products/products.module").then((m) => m.ProductsModule),
+  },
 ];
 
 @NgModule({
