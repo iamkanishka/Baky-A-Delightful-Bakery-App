c741e396b23d1bed1a2c6b46be24cd415b57ceb0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-21 18:43:56 +0530

    chore: Added route to  ProductDetailComponent and ProductsListComponent

--- src/app/pages/products/products-routing.module.ts
+++ src/app/pages/products/products-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from '@nativescript/angular';
+import { ProductsListComponent } from "./products-list/products-list.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  //  {path: "", component: ProductsListComponent },
+  {path: "", component: ProductDetailComponent }
+];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forChild(routes)],
