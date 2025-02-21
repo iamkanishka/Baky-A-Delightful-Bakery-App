37aae4cae1bba1fa71f1833421cd43ad41479cf9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-21 18:39:03 +0530

    feat: Hide ActionBar

--- src/app/pages/products/products-list/products-list.component.ts
+++ src/app/pages/products/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { RouterExtensions } from "@nativescript/angular";
+import { Page } from "@nativescript/core";
 
 @Component({
   selector: "app-products-list",
@@ -6,4 +8,10 @@ import { Component } from "@angular/core";
   templateUrl: "./products-list.component.html",
   styleUrl: "./products-list.component.scss",
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    constructor(private router: RouterExtensions, private _page: Page,) {
+ 
+      this._page.actionBarHidden = true;
+      
+    }
+}
