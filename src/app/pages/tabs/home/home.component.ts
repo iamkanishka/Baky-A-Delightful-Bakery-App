3eff2753d8a35f5b910b4d22f1a140ef24b395e9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-21 18:34:06 +0530

    chore: Removed featuredProducts sample data

--- src/app/pages/tabs/home/home.component.ts
+++ src/app/pages/tabs/home/home.component.ts
@@ -13,33 +13,6 @@ export class HomeComponent {
     this._page.actionBarHidden = true;
   }
 
-
-  featuredProducts = [
-    {
-      image: "https://zukachocolates.com/wp-content/uploads/2021/03/FULLY-LOADED-CHOCOLATE-TRUFFLE-CAKE-1.jpg",
-      rating: 4.8,
-      name: "Chocolate Cake",
-      discountedPrice: 25.0,
-      originalPrice: 30.0,
-    },
-    {
-      image: "https://bluebowlrecipes.com/wp-content/uploads/2023/08/chocolate-truffle-cake-8844.jpg",
-      rating: 4.8,
-      name: "Choco Cookies",
-      discountedPrice: 15.0,
-      originalPrice: 20.0,
-    },
-    {
-      image: "https://justbakedcake.com/wp-content/uploads/2020/11/Chocolate-Truffle-Gateaux.jpg",
-      rating: 4.7,
-      name: "Blueberry Muffin",
-      discountedPrice: 18.0,
-      originalPrice: 22.0,
-    },
-  ];
-
-
-
   public naviagteToCategories() {
     this.router.navigate([
       "/tabs",
