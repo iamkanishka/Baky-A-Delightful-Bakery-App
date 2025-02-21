97393cf1c676258347a8023213b9451b1109f775
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-21 21:41:03 +0530

    chore: Hide ActionBar

--- src/app/pages/products/product-gallery/product-gallery.component.ts
+++ src/app/pages/products/product-gallery/product-gallery.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-product-gallery',
-  imports: [],
   templateUrl: './product-gallery.component.html',
   styleUrl: './product-gallery.component.scss'
 })
 export class ProductGalleryComponent {
-
+ constructor( private _page: Page) {
+    this._page.actionBarHidden = true;
+  }
 }
