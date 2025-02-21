2d1a48d0c60db8c178f1c4ff0dd4e6115b2528ab
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-21 21:40:09 +0530

    chore: Declared ProductGalleryComponent

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -4,9 +4,10 @@ import { NativeScriptCommonModule } from "@nativescript/angular";
 import { ProductsRoutingModule } from "./products-routing.module";
 import { ProductsListComponent } from "./products-list/products-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ProductGalleryComponent } from "./product-gallery/product-gallery.component";
 
 @NgModule({
-  declarations: [ProductsListComponent, ProductDetailComponent],
+  declarations: [ProductsListComponent, ProductDetailComponent, ProductGalleryComponent],
   imports: [NativeScriptCommonModule, ProductsRoutingModule], 
    schemas: [NO_ERRORS_SCHEMA],
 })
