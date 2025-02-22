380085e59750c8ec3bf354376a8e6b3eb406666f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-22 21:51:49 +0530

    BakeriesComponent init

---
+++ src/app/pages/bakeries/bakeries/bakeries.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bakeries',
+  imports: [],
+  templateUrl: './bakeries.component.html',
+  styleUrl: './bakeries.component.scss'
+})
+export class BakeriesComponent {
+
+}
