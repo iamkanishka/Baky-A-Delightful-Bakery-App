37103aae2c10ec4030ea7c50d07e7687bbfefaf1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-22 21:55:27 +0530

    ExploreBakeriesComponent init

---
+++ src/app/pages/tabs/explore/explore-bakeries/explore-bakeries.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-bakeries',
+  imports: [],
+  templateUrl: './explore-bakeries.component.html',
+  styleUrl: './explore-bakeries.component.scss'
+})
+export class ExploreBakeriesComponent {
+
+}
