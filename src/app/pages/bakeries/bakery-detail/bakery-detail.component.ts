fb20d216a67e035ca9b9b3974a606c7385fbd384
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-22 21:52:00 +0530

    BakeryDetailComponent init

---
+++ src/app/pages/bakeries/bakery-detail/bakery-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bakery-detail',
+  imports: [],
+  templateUrl: './bakery-detail.component.html',
+  styleUrl: './bakery-detail.component.scss'
+})
+export class BakeryDetailComponent {
+
+}
