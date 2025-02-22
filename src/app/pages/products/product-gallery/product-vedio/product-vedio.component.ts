422914fe05ffd68127cc0da09edc9bfb22ea2425
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-22 21:54:49 +0530

    ProductVedioComponent init

---
+++ src/app/pages/products/product-gallery/product-vedio/product-vedio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-vedio',
+  imports: [],
+  templateUrl: './product-vedio.component.html',
+  styleUrl: './product-vedio.component.scss'
+})
+export class ProductVedioComponent {
+
+}
