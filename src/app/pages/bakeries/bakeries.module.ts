7cf715b2945d97aa85f8879cf761c9dbe745650e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-22 21:49:39 +0530

    BakeriesModule init

---
+++ src/app/pages/bakeries/bakeries.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BakeriesRoutingModule } from './bakeries-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BakeriesRoutingModule
+  ]
+})
+export class BakeriesModule { }
