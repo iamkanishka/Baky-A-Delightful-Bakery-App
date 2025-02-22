bd2162d9623929c4aa93801ace521157fd655a72
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-22 21:49:12 +0530

    BakeryCardComponent init

---
+++ src/app/components/bakery-card/bakery-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bakery-card',
+  imports: [],
+  templateUrl: './bakery-card.component.html',
+  styleUrl: './bakery-card.component.scss'
+})
+export class BakeryCardComponent {
+
+}
