642432cfc0a803462aa0d90d558e1b736a460ea7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-23 21:35:34 +0530

    RecentSearchComponent init

---
+++ src/app/pages/tabs/home/recent-search/recent-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-search',
+  imports: [],
+  templateUrl: './recent-search.component.html',
+  styleUrl: './recent-search.component.scss'
+})
+export class RecentSearchComponent {
+
+}
