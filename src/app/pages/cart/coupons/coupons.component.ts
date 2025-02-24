dd29525823227959aec5b21495610a2a84895d9d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-24 21:28:27 +0530

    feat: coupons sample data

--- src/app/pages/cart/coupons/coupons.component.ts
+++ src/app/pages/cart/coupons/coupons.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-coupons',
-  imports: [],
+ 
   templateUrl: './coupons.component.html',
   styleUrl: './coupons.component.scss'
 })
 export class CouponsComponent {
-
+  coupons = [
+    { code: 'WELCOME200', description: 'Add items worth $40 more to unlock', offer: 'Get 50% OFF' },
+    { code: 'CASHBACK12', description: 'Add items worth $50 more to unlock', offer: 'Up to $12.00 cashback' },
+    { code: 'FEST2COST', description: 'Add items worth $25 more to unlock', offer: 'Get 50% OFF for Combo' },
+    { code: 'WELCOME50', description: 'Add items worth $100 more to unlock', offer: 'Get 10% OFF' },
+  ];
 }
