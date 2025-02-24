bd09b8df9817ecbe2ac9be495a8225844a862c3e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-24 21:30:19 +0530

    CouponsCardComponent init

---
+++ src/app/components/coupons-card/coupons-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coupons-card',
+  imports: [],
+  templateUrl: './coupons-card.component.html',
+  styleUrl: './coupons-card.component.scss'
+})
+export class CouponsCardComponent {
+
+}
