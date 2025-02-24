efee9dc896c168c642b29bc3854d67a57916bad2
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-24 21:17:59 +0530

    feat: RecentSearchComponent logics with sample data

--- src/app/pages/tabs/home/recent-search/recent-search.component.ts
+++ src/app/pages/tabs/home/recent-search/recent-search.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recent-search',
-  imports: [],
   templateUrl: './recent-search.component.html',
-  styleUrl: './recent-search.component.scss'
+  styleUrls: ['./recent-search.component.css'],
 })
 export class RecentSearchComponent {
+  recentSearches: string[] = [
+    'Raspberry Rhapsody',
+    'Berry Burst Bonanza',
+    'Velvet Dream Delight'
+  ];
 
+  recentViews = [
+    { name: 'Chocolate Chip Cascade', category: 'Cookies', price: 20, image: '~/images/cookie.jpg' },
+    { name: 'Choco Bliss Cupcakes', category: 'Cup Cake', price: 22, image: '~/images/cupcake.jpg' },
+    { name: 'Strawberry Fields Frosted', category: 'Donut', price: 25, image: '~/images/donut.jpg' },
+    { name: 'Chocolate Caramel Donuts', category: 'Donut', price: 16, image: '~/images/donut2.jpg' },
+  ];
 }
