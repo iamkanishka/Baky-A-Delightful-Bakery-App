184ba494e67f6e07b37c71d38907e78df39c3fc3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-25 10:52:55 +0530

    feat: Declared TrackOrderComponent, TrackLiveLocationComponent

--- src/app/pages/orders/orders.module.ts
+++ src/app/pages/orders/orders.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { NativeScriptCommonModule } from '@nativescript/angular';
 import { OrdersRoutingModule } from './orders-routing.module';
-
-
+import {TrackLiveLocationComponent} from './track-live-location/track-live-location.component';
+import {TrackOrderComponent} from './track-order/track-order.component';
+ 
 @NgModule({
-  declarations: [],
+  declarations: [TrackOrderComponent, TrackLiveLocationComponent ],
   imports: [
-    CommonModule,
+    NativeScriptCommonModule,
     OrdersRoutingModule
   ]
 })
