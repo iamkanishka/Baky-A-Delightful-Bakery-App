4bb3c374a6f3d133fdc9fd2907d65f3008952fd3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-25 11:14:25 +0530

    TrackLiveLocationComponent init

---
+++ src/app/pages/orders/track-live-location/track-live-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-live-location',
+  imports: [],
+  templateUrl: './track-live-location.component.html',
+  styleUrl: './track-live-location.component.scss'
+})
+export class TrackLiveLocationComponent {
+
+}
