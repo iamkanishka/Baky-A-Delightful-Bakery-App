081da528df1a0b3d41411a4bd8e12049ad589173
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-26 21:25:04 +0530

    EReceiptComponent init

---
+++ src/app/pages/checkout/e-receipt/e-receipt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-receipt',
+  imports: [],
+  templateUrl: './e-receipt.component.html',
+  styleUrl: './e-receipt.component.scss'
+})
+export class EReceiptComponent {
+
+}
