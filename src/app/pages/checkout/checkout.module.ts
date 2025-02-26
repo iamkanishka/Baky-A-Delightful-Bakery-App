2b4468d472675445fafec5050f1cf6044a34ae2f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-26 21:25:25 +0530

    chore: Added EReceiptComponent to CheckoutModule declarations

--- src/app/pages/checkout/checkout.module.ts
+++ src/app/pages/checkout/checkout.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { NativeScriptModule } from '@nativescript/angular'
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { ShippingAddressComponent } from './shipping-address/shipping-address.component';
+import { EReceiptComponent } from './e-receipt/e-receipt.component';
 
 
 @NgModule({
-  declarations: [ShippingAddressComponent],
+  declarations: [ShippingAddressComponent, EReceiptComponent],
   imports: [
     NativeScriptModule,
     CheckoutRoutingModule
