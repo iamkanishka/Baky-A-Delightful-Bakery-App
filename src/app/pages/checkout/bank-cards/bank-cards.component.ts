66dd7f892dfdc446913124044657ef4d78376bf5
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-26 21:30:53 +0530

    BankCardsComponent init

---
+++ src/app/pages/checkout/bank-cards/bank-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-cards',
+  imports: [],
+  templateUrl: './bank-cards.component.html',
+  styleUrl: './bank-cards.component.scss'
+})
+export class BankCardsComponent {
+
+}
