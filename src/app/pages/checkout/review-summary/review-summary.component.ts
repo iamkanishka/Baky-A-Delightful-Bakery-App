3c707220a41bd22fa102907aceb7ac21a58b6dba
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-26 21:26:45 +0530

    ReviewSummaryComponent init

---
+++ src/app/pages/checkout/review-summary/review-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-summary',
+  imports: [],
+  templateUrl: './review-summary.component.html',
+  styleUrl: './review-summary.component.scss'
+})
+export class ReviewSummaryComponent {
+
+}
