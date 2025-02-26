deef8944293f15443b4cdea07409427f90a65b63
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-26 21:31:22 +0530

    chore: Update checkout module to include BankCardsComponent

--- src/app/pages/checkout/checkout.module.ts
+++ src/app/pages/checkout/checkout.module.ts
@@ -1,19 +1,22 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
- 
-import { NativeScriptModule } from '@nativescript/angular'
-import { CheckoutRoutingModule } from './checkout-routing.module';
-import { ShippingAddressComponent } from './shipping-address/shipping-address.component';
-import { EReceiptComponent } from './e-receipt/e-receipt.component';
-import { ReviewSummaryComponent } from './review-summary/review-summary.component';
-import { PaymentMethodsComponent } from './payment-methods/payment-methods.component';
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
+import { NativeScriptModule } from "@nativescript/angular";
+import { CheckoutRoutingModule } from "./checkout-routing.module";
+import { ShippingAddressComponent } from "./shipping-address/shipping-address.component";
+import { EReceiptComponent } from "./e-receipt/e-receipt.component";
+import { ReviewSummaryComponent } from "./review-summary/review-summary.component";
+import { PaymentMethodsComponent } from "./payment-methods/payment-methods.component";
+import { BankCardsComponent } from "./bank-cards/bank-cards.component";
 
 @NgModule({
-  declarations: [ShippingAddressComponent, EReceiptComponent, ReviewSummaryComponent, PaymentMethodsComponent],
-  imports: [
-    NativeScriptModule,
-    CheckoutRoutingModule
+  declarations: [
+    ShippingAddressComponent,
+    EReceiptComponent,
+    ReviewSummaryComponent,
+    PaymentMethodsComponent,
+    BankCardsComponent
   ],
-  schemas:[NO_ERRORS_SCHEMA]
+  imports: [NativeScriptModule, CheckoutRoutingModule],
+  schemas: [NO_ERRORS_SCHEMA],
 })
-export class CheckoutModule { }
+export class CheckoutModule {}
