0a520c679a1a012dfe9ddc19ef75bcdd2b706d32
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-26 21:27:09 +0530

    chore: Added ReviewSummaryComponent to CheckoutModule declarations and imports, and added NO_ERRORS_SCHEMA to the module.

--- src/app/pages/checkout/checkout.module.ts
+++ src/app/pages/checkout/checkout.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
  
 import { NativeScriptModule } from '@nativescript/angular'
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { ShippingAddressComponent } from './shipping-address/shipping-address.component';
 import { EReceiptComponent } from './e-receipt/e-receipt.component';
+import { ReviewSummaryComponent } from './review-summary/review-summary.component';
 
 
 @NgModule({
-  declarations: [ShippingAddressComponent, EReceiptComponent],
+  declarations: [ShippingAddressComponent, EReceiptComponent, ReviewSummaryComponent],
   imports: [
     NativeScriptModule,
     CheckoutRoutingModule
-  ]
+  ],
+  schemas:[NO_ERRORS_SCHEMA]
 })
 export class CheckoutModule { }
