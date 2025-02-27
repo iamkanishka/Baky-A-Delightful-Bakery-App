c54d039779f797d501c014c910bf01f2d4a434c6
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-27 21:44:13 +0530

    AddMoneyComponent init

---
+++ src/app/pages/wallet/topup-wallet/topup-wallet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topup-wallet',
+  imports: [],
+  templateUrl: './topup-wallet.component.html',
+  styleUrl: './topup-wallet.component.scss'
+})
+export class TopupWalletComponent {
+
+}
