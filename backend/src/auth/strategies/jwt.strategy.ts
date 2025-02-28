ecbdabae6c1b2c4d82c6adc26ab2cc7482b7145b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-28 18:32:13 +0530

    feat:  JwtStrategy with validate logic

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+AuthService
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'defaultSecret',
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    return this.authService.validateUser(payload);
+  }
+}
