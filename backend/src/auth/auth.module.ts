c28652b07e0b22aadaf63db4f6fadfb29116e940
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-28 18:18:09 +0530

    dir: Moved under auth dir

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   providers: [AuthService],
