72d9bf5448ed8b52c61e3445b0730352553226ea
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-28 18:39:02 +0530

    feat: Added authentication functionality to AuthController

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from 'src/guards/passport-local.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+  // Email/Password Login
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.body.email, req.body.password);
+  }
+}
