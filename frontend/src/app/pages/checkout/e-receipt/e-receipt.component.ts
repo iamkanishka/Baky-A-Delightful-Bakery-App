7c2b69f7d6d04906f62c5e8a579c1da596ff7358
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-01 23:19:04 +0530

    feat: Added logic

--- frontend/src/app/pages/checkout/e-receipt/e-receipt.component.ts
+++ frontend/src/app/pages/checkout/e-receipt/e-receipt.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterExtensions } from "@nativescript/angular";
 
 @Component({
   selector: 'app-e-receipt',
-  imports: [],
   templateUrl: './e-receipt.component.html',
   styleUrl: './e-receipt.component.scss'
 })
 export class EReceiptComponent {
+  constructor(private routerExtensions: RouterExtensions) {}
 
+  goBack() {
+    this.routerExtensions.back();
+  }
 }
