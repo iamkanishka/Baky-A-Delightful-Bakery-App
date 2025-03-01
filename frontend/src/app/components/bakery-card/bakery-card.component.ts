982d499c4194911137f32d6f6bd4b3148af03936
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-01 23:18:37 +0530

    feat: Added logics

--- frontend/src/app/components/bakery-card/bakery-card.component.ts
+++ frontend/src/app/components/bakery-card/bakery-card.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './bakery-card.component.scss'
 })
 export class BakeryCardComponent {
-
+  bakery = {
+    name: 'Rise & Shine Bakery Co.',
+    categories: 'Cup Cake, Cookies, Donuts, Breads, Cake,...',
+    rating: 4.8,
+    reviews: '1k+',
+    address: '8502 Preston Rd. Inglewood, Maine 98380',
+    distance: '15 min • 1.5km',
+    hours: 'Mon - Sun 11am - 11pm',
+    customers: '9,500+',
+    products: '250+',
+    avgRating: '4.9+',
+    totalReviews: '4,956',
+  }
 }
