9abeec77eabca2c79ea59522b0183cce6e7c2212
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-03 18:42:20 +0530

    chore: Removed imports

--- frontend/src/app/pages/tabs/bakeries/bakeries/bakeries.component.ts
+++ frontend/src/app/pages/tabs/bakeries/bakeries/bakeries.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bakeries',
-  imports: [],
   templateUrl: './bakeries.component.html',
   styleUrl: './bakeries.component.scss'
 })
