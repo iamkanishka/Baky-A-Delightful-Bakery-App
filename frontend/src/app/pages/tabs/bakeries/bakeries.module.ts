b25608a7fe8a86917f49f6cf86e189a0fce913b3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-03 18:44:32 +0530

    chore: Added map Module

--- frontend/src/app/pages/tabs/bakeries/bakeries.module.ts
+++ frontend/src/app/pages/tabs/bakeries/bakeries.module.ts
@@ -3,11 +3,13 @@ import { NativeScriptCommonModule } from "@nativescript/angular";
 import { BakeriesRoutingModule } from './bakeries-routing.module';
 import {BakeryDetailComponent} from './bakery-detail/bakery-detail.component'
 import {BakeriesComponent} from './bakeries/bakeries.component'
+import {MapComponent} from '../..//shared/map/map.component'
 @NgModule({
-  declarations: [BakeryDetailComponent, BakeriesComponent ],
+  declarations: [BakeryDetailComponent, BakeriesComponent],
   imports: [
     NativeScriptCommonModule,
     BakeriesRoutingModule,
+    MapComponent
     
   ]
 })
