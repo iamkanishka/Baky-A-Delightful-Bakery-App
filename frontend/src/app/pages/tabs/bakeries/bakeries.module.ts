6dc113af47366f10a50b0130cb3d24785b4fa617
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-03 18:43:00 +0530

    chore: Declared BakeryDetailComponent

--- frontend/src/app/pages/tabs/bakeries/bakeries.module.ts
+++ frontend/src/app/pages/tabs/bakeries/bakeries.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NativeScriptCommonModule } from "@nativescript/angular";
 import { BakeriesRoutingModule } from './bakeries-routing.module';
+import {BakeryDetailComponent} from './bakery-detail/bakery-detail.component'
  
 @NgModule({
-  declarations: [],
+  declarations: [BakeryDetailComponent],
   imports: [
     NativeScriptCommonModule,
     BakeriesRoutingModule,
