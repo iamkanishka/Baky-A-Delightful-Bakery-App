0d2546635d26f0cd927cb8bed9293afa244f2944
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-03 21:39:18 +0530

    feat: Added sample bakeries data

--- frontend/src/app/pages/tabs/bakeries/bakeries/bakeries.component.ts
+++ frontend/src/app/pages/tabs/bakeries/bakeries/bakeries.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './bakeries.component.scss'
 })
 export class BakeriesComponent {
-
+  bakeries = [
+    {
+      name: "Rise & Shine Bakery Co.",
+      description: "Cup Cake, Cookies, Donuts, Breads, Cake..",
+      address: "8502 Preston Rd. Inglewood, Maine 98380",
+      image: "~/images/bakery1.jpg",
+    },
+    {
+      name: "Sweet Confections Cottage",
+      description: "Cup Cake, Cookies, Donuts, Breads, Cake..",
+      address: "4140 Parker Rd. Allentown, New Mexico 31134",
+      image: "~/images/bakery2.jpg",
+    },
+  ];
 }
